f46effc87f155a05b83913b121683050bad015e1
martinemoyano <memoyano@gmail.com>
2022-08-04 07:42:58 -0300

    todas las funciones ok

--- arrays.js
+++ arrays.js
@@ -1,79 +1,68 @@
+// let nuevoIngreso;
+// let buscar;
 
-// }}
-// ARRAY CON NOMBRES DE 5 MASCOTAS
-// let entradaNombre = prompt('nombre de la mascota')
-// let cantidad = 5;
-// const listaMascotas = [];
-
-// function agregarMascota() {
-  
-//     mascota = prompt('nombre de mascota para agregar a la lista')
-//     listaMascotas.push(mascota)
-//     console.log(listaMascotas)
-// }
-
-// while(listaMascotas.length != cantidad){
-//     agregarMascota();
-// }
-// {  
-//     console.log("la lista de " + cantidad + " mascotas es: " + listaMascotas)
-// }
-let nuevoIngreso;
-let buscar;
-
-class mascota{
-    constructor(nombre,raza, edad, peso){
-    this.nombre = nombre;
-    this.raza = raza;
-    this.edad = parseInt(edad);
-    this.peso = parseInt(peso);
-    }}
-
+class mascota {
+    constructor(nombre, raza, edad, peso) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.edad = parseInt(edad);
+        this.peso = parseInt(peso);
+    }
+}
+// LISTA ORIGINAL DE MASCOTAS
 const mascotas = []
-mascotas.push(new mascota("Bobi", "bulldog", 3, 11));
-mascotas.push(new mascota("Firulai", "golden", 2, 9));
+mascotas.push(new mascota("bobi", "bulldog", 3, 11));
+mascotas.push(new mascota("firulai", "golden", 2, 9));
 mascotas.push(new mascota("colita", "labrador", 6, 13));
 mascotas.push(new mascota("toto", "poodle", 4, 6));
 mascotas.push(new mascota("braulio", "pastor", 7, 16));
-
-function mascotaNueva(){
-    this.nombre=prompt("nombre de la mascota");
-    this.raza=prompt("ingrese raza");
-    this.peso=parseInt(prompt("ingrese el peso"))
-    this.edad=parseInt(prompt("ingrese edad"))
-    mascotas.push(new mascota(this.nombre,this.raza,this.peso,this.edad))
-    console.log(mascotas); 
+// FUNCION PARA INGRESAR UNA MASCOTA NUEVA AL ARRAY MASCOTAS
+function mascotaNueva() {
+    this.nombre = prompt("nombre de la mascota");
+    this.raza = prompt("ingrese raza");
+    this.peso = parseInt(prompt("ingrese el peso"))
+    this.edad = parseInt(prompt("ingrese edad"))
+    mascotas.push(new mascota(this.nombre, this.raza, this.peso, this.edad))
+    console.log(mascotas);
 }
 
+// FUNCIONA. TIENE UNA CONFIRMACION AL PRINCIPIO PARA ASEGURAR QUE SE QUIERE AGREGAR UNA NUEVA MASCOTA
+function agregarMascota() {
+    nuevoIngreso = prompt("desea agregar una nueva mascota? escriba 'si' para sumarla")
+    if (nuevoIngreso == "si") {
+        mascotaNueva();
+        console.log(this.nombre + "ha sido agregado a la lista");
+        console.log(mascotas);
+    } else {
+        alert("ninguna mascota ingeresada");
+    }
+}
+
+//  FUNCIONA
+function mostrarLista() {
+    for (const mascota of mascotas) {
+        console.log(mascota.nombre);
+        
+    }cantidadMascotas();
+}
 
 // FUNCIONA
-function agregarMascota(){
-nuevoIngreso=prompt("desea agregar una nueva mascota? escriba 'si' para sumarla")
-if (nuevoIngreso=="si"){
-    mascotaNueva();
-    console.log(this.nombre + "ha sido agregado a la lista");
-    console.log(mascotas);
-}else{
-alert("ninguna mascota ingeresada")
-}}
-// NO FUNCIONA  SIEMPRE DA RESULTADO TRUE
-function buscarMascota(){
-buscar=prompt("escriba el nombre de la mascota a buscar")
-console.log(mascotas.includes(buscar))
-   
-      if(true) {
-          alert(buscar + " está en la lista.")
-   }
-   else{
-       console.log(buscar + "no está en la lista")
-   }
-console.log("gracias");}
-
-// NO FUNCIONA
-function mostrarLista(){
-    console.log(mascotas.nombre);
+function cantidadMascotas() {
+    console.log("hay "+ mascotas.length + " mascotas en la lista")
 }
 
-function cantidadMascotas(){
-    console.log(mascotas.length)
+// FUNCIONA  
+function buscarMascota()
+{
+    let buscar=prompt("escriba el nombre de la mascota a buscar");
+    const resultado = mascotas.find((el)=> el.nombre==buscar);
+    console.log(resultado);
+}
+
+// FUNCIONA
+function buscarRaza()
+{
+    let buscarPorRaza=prompt("ingrese la raza");
+    const resultado2 = mascotas.filter((el)=> el.raza==buscarPorRaza);
+    console.log(resultado2);
 }
